c31aeec6d617a13368e9081c88671f969b9401b9
garduino <gsa@arsol.net>
2015-06-30 09:07:07 -0300

    Initial commit

---
+++ Meteor-Leaderboard.js
@@ -0,0 +1,39 @@
+
+if(Meteor.isClient){ 
+	
+ Template.leaderboard.helpers({
+    'player': function(){
+        return PlayersList.find()
+    },
+   'selectedClass': function(){ 
+   	   var playerId = this._id;
+   	   var selectedPlayer = Session.get('selectedPlayer');
+   	   if(playerId == selectedPlayer){
+   	   return "selected" }
+   }  
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+        var playerId = this._id; 
+        Session.set('selectedPlayer', playerId);
+        // var selectedPlayer = Session.get('selectedPlayer');
+        // console.log(selectedPlayer);
+    },
+       'dblclick .player': function(){
+      console.log("You DOUBLE clicked a .player element");
+    },
+       'mouseover .player': function(){
+      console.log("The mouse is over a .player element");
+    }
+    
+  });
+	
+		
+}
+
+
+if(Meteor.isServer){ 
+}
+
+PlayersList = new Mongo.Collection('players');
